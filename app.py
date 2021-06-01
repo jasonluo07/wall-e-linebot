58de30fbba2468d991a208229b00214aa2092dd6
yiwen <tfg10211616@gmail.com>
2021-06-01 21:02:31 +0800

    換行測試

--- app.py
+++ app.py
@@ -64,7 +64,11 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, reply_message)
     # 類別選項
     elif '類別' == msg:
-        reply_message = TextSendMessage(text='請選擇以下類別： 1 展覽表演 2 品牌活動 3 論壇講座 4 市集活動') # 待研究 文字怎麼換行
+        reply_message = TextSendMessage(text='請選擇以下類別：'
+        '\n''1 展覽表演' 
+        '2 品牌活動' 
+        '3 論壇講座' 
+        '4 市集活動') # 待研究 文字怎麼換行
         line_bot_api.reply_message(event.reply_token, reply_message)
     # 回傳位置訊息
     elif '華山' == msg:
