2c5fd14633f9418e56e2e279bf9f6715dd3cb507
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-06-04 13:56:18 +0800

    tweak

--- app.py
+++ app.py
@@ -30,6 +30,74 @@ line_bot_api = LineBotApi(
     'I5hgulT9ZKbbAFqbzumFaVYS0jgUmuyG5JVmFgrq+HlPngNiK05bfvgsiBzAxOAjwE3FAc5olRj+iXwglbNuz6Qpp0YAW9z/Mq72Ea+96SzEMbp6KChDbEg74sa4c433HzVMB59OUPcK45d9l6n1uQdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('be58b1568a2dbd2327a5c6f6bd48e80a')
 
+# 實裝中
+
+
+def sendDatetime(event):  # 日期時間
+    try:
+        reply_message = TemplateSendMessage(
+            alt_text='日期時間範例',
+            template=ButtonsTemplate(
+                thumbnail_image_url='https://i.imgur.com/VxVB46z.jpg',
+                title='日期時間示範',
+                text='請選擇：',
+                actions=[
+                    DatetimePickerTemplateAction(
+                        label="選取日期",
+                        data="action=sell&mode=date",  # 觸發postback事件
+                        mode="date",  # 選取日期
+                        initial="2020-10-01",  # 顯示初始日期
+                        min="2020-10-01",  # 最小日期
+                        max="2021-12-31"  # 最大日期
+                    ),
+                    DatetimePickerTemplateAction(
+                        label="選取時間",
+                        data="action=sell&mode=time",
+                        mode="time",  # 選取時間
+                        initial="10:00",
+                        min="00:00",
+                        max="23:59"
+                    ),
+                    DatetimePickerTemplateAction(
+                        label="選取日期時間",
+                        data="action=sell&mode=datetime",
+                        mode="datetime",  # 選取日期時間
+                        initial="2020-10-01T10:00",
+                        min="2020-10-01T00:00",
+                        max="2021-12-31T23:59"
+                    )
+                ]
+            )
+        )
+        return reply_message
+    except:
+        reply_message = TextSendMessage(
+            text='發生錯誤！'
+        )
+        return reply_message
+
+
+def sendData_sell(event, backdata):  # Postback,顯示日期時間
+    try:
+        if backdata.get('mode') == 'date':
+            dt = '日期為：' + event.postback.params.get('date')  # 讀取日期
+        elif backdata.get('mode') == 'time':
+            dt = '時間為：' + event.postback.params.get('time')  # 讀取時間
+        elif backdata.get('mode') == 'datetime':
+            dt = datetime.datetime.strptime(event.postback.params.get(
+                'datetime'), '%Y-%m-%dT%H:%M')  # 讀取日期時間
+            dt = dt.strftime(
+                '{d}%Y-%m-%d, {t}%H:%M').format(d='日期為：', t='時間為：')  # 轉為字串
+        reply_message = TextSendMessage(
+            text=dt
+        )
+        return reply_message
+    except:
+        reply_message = TextSendMessage(
+            text='發生錯誤！'
+        )
+        return reply_message
+
 
 # 建立 callback 路由，檢查 LINE Bot 的資訊是否正確
 @app.route("/callback", methods=['POST'])
@@ -195,10 +263,76 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, reply_message)
 
 
+def sendDatetime(event):  # 日期時間
+    try:
+        reply_message = TemplateSendMessage(
+            alt_text='日期時間範例',
+            template=ButtonsTemplate(
+                thumbnail_image_url='https://i.imgur.com/VxVB46z.jpg',
+                title='日期時間示範',
+                text='請選擇：',
+                actions=[
+                    DatetimePickerTemplateAction(
+                        label="選取日期",
+                        data="action=sell&mode=date",  # 觸發postback事件
+                        mode="date",  # 選取日期
+                        initial="2020-10-01",  # 顯示初始日期
+                        min="2020-10-01",  # 最小日期
+                        max="2021-12-31"  # 最大日期
+                    ),
+                    DatetimePickerTemplateAction(
+                        label="選取時間",
+                        data="action=sell&mode=time",
+                        mode="time",  # 選取時間
+                        initial="10:00",
+                        min="00:00",
+                        max="23:59"
+                    ),
+                    DatetimePickerTemplateAction(
+                        label="選取日期時間",
+                        data="action=sell&mode=datetime",
+                        mode="datetime",  # 選取日期時間
+                        initial="2020-10-01T10:00",
+                        min="2020-10-01T00:00",
+                        max="2021-12-31T23:59"
+                    )
+                ]
+            )
+        )
+        return reply_message
+    except:
+        reply_message = TextSendMessage(
+            text='發生錯誤！'
+        )
+        return reply_message
+
+
+def sendData_sell(event, backdata):  # Postback,顯示日期時間
+    try:
+        if backdata.get('mode') == 'date':
+            dt = '日期為：' + event.postback.params.get('date')  # 讀取日期
+        elif backdata.get('mode') == 'time':
+            dt = '時間為：' + event.postback.params.get('time')  # 讀取時間
+        elif backdata.get('mode') == 'datetime':
+            dt = datetime.datetime.strptime(event.postback.params.get(
+                'datetime'), '%Y-%m-%dT%H:%M')  # 讀取日期時間
+            dt = dt.strftime(
+                '{d}%Y-%m-%d, {t}%H:%M').format(d='日期為：', t='時間為：')  # 轉為字串
+        reply_message = TextSendMessage(
+            text=dt
+        )
+        return reply_message
+    except:
+        reply_message = TextSendMessage(
+            text='發生錯誤！'
+        )
+        return reply_message
+
+
 # 日期時間按鈕會觸發 Postback 事件
 @handler.add(PostbackEvent)  # PostbackTemplateAction 觸發此事件
 def handle_postback(event):
-    backdata = dict(parse_qsl(event.postback.data))  # 取得data資料
+    backdata = dict(parse_qsl(event.postback.data))  # 取得 data 資料
     if backdata.get('action') == 'sell':  # 讀取 Postback 資料中名稱為「action」項目的值
         sendData_sell(event, backdata)
 
